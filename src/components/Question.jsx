66ae504b0e6b4e5bbfc5c4527b9b5dd651ef9d74
[nyun-nye] <[2001_11_7@naver.com]>
2024-07-04 13:58:58 +0900

    change

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -15,88 +15,88 @@ import { useNavigate } from 'react-router-dom';
 
 const questions = {
   1: {
-    title: '아무일도 없는 나른한 오후, 친구가 놀자며 카톡을 보냈다.',
+    title: '집에 왔더니 아무도 없다면?',
     image: qs1,
     type: 'EI',
-    A: '빠르게 준비를 마치고 친구를 만나러 나간다.',
-    B: '얼마만의 휴일인데... 집에서 휴식을 취한다.',
+    A: '깜깜하고 너무 외롭다.',
+    B: '드디어 나만의 시간! 행복하다.',
   },
   2: {
-    title: '소개팅을 갔는데 분위기가 너무 어색하다.',
+    title: '오랜만에 고등학교 동창회에 왔다!',
     image: qs2,
     type: 'EI',
-    A: '어색함을 견디지 못하고 말을 먼저 꺼낸다.',
-    B: '너무 어색해 미치겠지만 가만히 있는다.',
+    A: '오랜만에 만난 많은 친구들과 얘기한다.',
+    B: '많이 친했던 친구들과 얘기한다.',
   },
   3: {
-    title: '자취를 하려고 집을 구하러 다닌다.',
+    title: '새로운 노래를 들었다!',
     image: qs3,
     type: 'SN',
-    A: '방 구조, 수압, 방음 등을 꼼꼼하게 체크한다.',
-    B: '방이 화사한지, 넓은지, 포근한지 등을 본다.',
+    A: '멜로디에 집중한다.',
+    B: '가사에 집중한다.',
   },
   4: {
-    title: '바로 당장 내일이 시험인데 공부하기가 너무 싫다.',
+    title: '집에 가는 약도를 그려보자!',
     image: qs4,
     type: 'SN',
-    A: '범위가 줄었으면 좋겠다. 이번 시험 잘 봐야 하는데..',
-    B: '시험은 왜 봐야 하는거지..? 시험이 사라지는 시대도 올까?',
+    A: '짧고 간결하게! 큰 건물 위주로 그린다.',
+    B: '상세하게! 어디에 있는지까지 그린다.',
   },
   5: {
-    title: '화났을 때 나는?',
+    title: '늦어서 미안! 많이 기다렸어?',
     image: qs5,
     type: 'TF',
-    A: '논리적으로 잘 말하면서 따진다.',
-    B: '할말이 많지만 너무 분해서 눈물부터 난다.',
+    A: '왜 늦은거야? (이유가 있으면 풀린다.)',
+    B: '왜 늦은거야? (사과를 하면 풀린다.)',
   },
   6: {
-    title: '고민을 얘기하는 친구, 듣다보니 친구의 잘못인 것 같다.',
+    title: '누군가가 나를 싫어한다는 소리를 들었을 때 나의 반응은?',
     image: qs6,
     type: 'TF',
-    A: '친구의 잘못된 점을 말해준다.',
-    B: '직접적으로 말하면 친구가 상처받을까봐 돌려 말한다.',
+    A: '나를? 왜? (순수하게 이유가 궁금하다.)',
+    B: '내가 뭐 잘못했나...?',
   },
   7: {
-    title: '준비물을 챙겨야할 때 나는?',
+    title: '과제를 할 때!',
     image: qs7,
     type: 'PJ',
-    A: '굳이 하루전에...? 당일날 챙기면 충분하다!',
-    B: '며칠 전에 미리 챙겨 준비해둔다!',
+    A: '자료조사부터 해봐야 틀이 잡힌다.',
+    B: '뭐부터 해야할지 계획을 짜야 틀이 잡힌다.',
   },
   8: {
-    title: '여행 계획을 짤 때 나는?',
+    title: '너 지금 뭐해? 심심한데 우리 잠깐 보자!',
     image: qs8,
     type: 'PJ',
-    A: '그때 그때 가고 싶은 곳 가고 먹고 싶은 것 먹는 것이 낭만이지!',
-    B: '버스 시간, 가격, 메뉴까지 미리 다 정해서 계획을 짜둔다.',
+    A: '너무 좋지! 지금 나갈게~!',
+    B: '엥? 갑자기? 왜?',
   },
   9: {
-    title: '곧 워터밤 페스티벌이 열린다고 한다.',
+    title: '취미 생활을 즐길 때 나는?',
     image: qs9,
     type: 'EI',
-    A: '아껴뒀던 수영복과 물총을 꺼내 페스티벌을 즐기러 간다.',
-    B: '집에서도 충분히 즐길 수 있어! 유튜브로 시청한다.',
+    A: '많은 사람들과 함께할 수 있는 스포츠, 문화생활을 선호한다.',
+    B: '나 혼자서 즐길 수 있는 보석십자수, 피포페인팅을 선호한다.',
   },
   10: {
-    title: '비행기를 탔는데 갑자기 흔들린다.',
+    title: '영화를 보고 난 후 친구에게 설명을 할 때!',
     image: qs10,
     type: 'SN',
-    A: '최대한 침착하며 안전벨트를 꽉 매고 주위를 살핀다.',
-    B: '비행기가 갑자기 추락하면 어쩌지?? 혹시 누가 테러를 일으킨 건 아닐까??',
+    A: '이 영화에서는 누가 죽고~ 누가 나오고~ ...',
+    B: '이 영화 분위기가 진짜 낭만적이었어!',
   },
   11: {
-    title: '친구가 갑자기 차 사고가 났다고 한다.',
+    title: '친구가 "나 목이 너무 아파"라고 했을 때 내 대답은?',
     image: qs11,
     type: 'TF',
-    A: '헐 보험은 들어놨지?? 보험사에서는 뭐래??',
-    B: '헐 몸은 괜찮아?? 다친데는 없고...?',
+    A: '목 어디? 뼈?',
+    B: '괜찮아? 많이 아파?',
   },
   12: {
-    title: '오랜만에 밖에서 외식을 하려고 한다.',
+    title: '내 휴대폰에는?',
     image: qs12,
     type: 'PJ',
-    A: '같은 곳은 지겨워! 새로운 곳을 탐방한다!',
-    B: '매번 가던 곳으로! 편안하고 맛있어서 좋다.',
+    A: '읽지 않은 알람이 많다.',
+    B: '읽지 않은 알람이 없다.',
   },
 };
 
@@ -120,7 +120,7 @@ function Question({num, setNum, updateScore}) {
         <div className="content" id="question">
             <p id="pgrNum">{num}/12</p>
             <div className="progress">
-                <div className="progress-bar progress-bar-striped progress-bar-animated bg-warning"
+                <div className="progress-bar progress-bar-striped progress-bar-animated bg-primary"
                 style={{width: `calc(100/12*${num}%)`}}
                 ></div>
             </div>
@@ -130,10 +130,10 @@ function Question({num, setNum, updateScore}) {
                     <img src={currentQuestion.image} className="question-image" />
                 )}
             </div>
-            <button className="btn btn-warning" onClick={()=>handleAnswer(currentQuestion.type,1)}>
+            <button className="btn btn-primary" onClick={()=>handleAnswer(currentQuestion.type,1)}>
                 {currentQuestion.A}
             </button>
-            <button className="btn btn-warning mt-3" onClick={()=>handleAnswer(currentQuestion.type,0)}>
+            <button className="btn btn-primary mt-3" onClick={()=>handleAnswer(currentQuestion.type,0)}>
                 {currentQuestion.B}
             </button>
         </div>
--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import bannerImage from '../image/짱구배너.png';
+import bannerImage from '../image/배너.png';
 
 function Start() {
   const navigate = useNavigate();
 
   return (
     <div className="content" id="start">
-      <p>나와 닮은 짱구 캐릭터 테스트</p>
-      <img src={bannerImage} alt="짱구단체" className="img1" />
-      <p id="maker">제작자: 이나경</p>
+      <p>인사이드아웃 캐릭터 테스트</p>
+      <img src={bannerImage} alt="배너" className="img1" />
+      <p id="maker">제작자: 윤예진</p>
       <p id="require">
-        ※단순히 재미로 제작한 것이며, 오수나 훈이가 나와도 화내지 말아주세요!
+        나의 인사이드 아웃 캐릭터 알아보기!
       </p>
-      <button className="btn btn-warning" onClick={() => navigate('/question')}>
-        떡잎마을로 출발~
+      <button className="btn btn-primary" onClick={() => navigate('/question')}>
+        라일리 머릿속으로~
       </button>
     </div>
   );
